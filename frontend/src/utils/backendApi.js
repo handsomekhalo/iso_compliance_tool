44d6825e33f522af0c1c516ce486a706e57c453d
Titus <handsomekhalo@gmail.com>
2025-12-08 15:42:22 +0200

    fixing frontend for deployment

--- frontend/src/utils/backendApi.js
+++ frontend/src/utils/backendApi.js
@@ -7,7 +7,8 @@ const backendApi = axios.create({
   // baseURL:'http://3.144.218.251',
  
   // baseURL: "http://127.0.0.1:8000",
-  baseURL: 'crossover.proxy.rlwy.net/',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+
     // 👈 Public IP of your EC2 instance
   withCredentials: true,          // Ensures cookies (like CSRF token) are sent
   headers: {
--- iso_compliance/settings.py
+++ iso_compliance/settings.py
@@ -238,14 +238,14 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS Settings (for Next.js frontend)
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://iso-compliance-tool-d8326zelr-titus-projects-cdab7abf.vercel.app",
+    "https://iso-compliance-tool.vercel.app/",
 
         # Your Vercel domain
 ]
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://iso-compliance-tool-d8326zelr-titus-projects-cdab7abf.vercel.app"
+    "https://iso-compliance-tool.vercel.app/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
