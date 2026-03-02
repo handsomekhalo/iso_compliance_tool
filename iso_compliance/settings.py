aab127b15ef8389419632d4a817a1fa55acddc76
Titus <handsomekhalo@gmail.com>
2026-03-02 10:45:51 +0200

    updated deployments

--- iso_compliance/settings.py
+++ iso_compliance/settings.py
@@ -77,6 +77,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
+
 ]
 
 ROOT_URLCONF = 'iso_compliance.urls'
